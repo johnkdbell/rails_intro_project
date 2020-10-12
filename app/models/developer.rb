35ddd29760341ed6c31e714f2cc4c2ca8db97b28
johnkdbell <johnkbell@hotmail.com>
2020-10-11 20:49:02 -0500

    Add validation to Game and Developer models

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,3 +1,4 @@
 class Developer < ApplicationRecord
+  has_many :games
   validates :name, presences: true, uniqueness: true
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :developer
+
+  validates :name, presence: true
+  validates :rating, :total_rating, numericality: true
+
 end
