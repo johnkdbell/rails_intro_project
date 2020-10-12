8ac884a524bcb19507ead344b929d57524090685
johnkdbell <johnkbell@hotmail.com>
2020-10-11 22:54:21 -0500

    Developer and Game pages displays data

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.includes(:developer).order("rating DESC").all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'developers/index'
-  get 'developers/show'
-  get 'games/index'
-  get 'games/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games, only: [:index, :show]
+  resources :developers, only: [:index, :show]
 end
