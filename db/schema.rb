2914b231712827b3e0d30601dd4cf13a40c6f6d1
johnkdbell <johnkbell@hotmail.com>
2020-10-23 10:32:46 -0500

    Create Genre and GameGenre models and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_231305) do
+ActiveRecord::Schema.define(version: 2020_10_23_152934) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "game_genres", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_game_genres_on_game_id"
+    t.index ["genre_id"], name: "index_game_genres_on_genre_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -30,5 +39,13 @@ ActiveRecord::Schema.define(version: 2020_10_11_231305) do
     t.index ["developer_id"], name: "index_games_on_developer_id"
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "game_genres", "games"
+  add_foreign_key "game_genres", "genres"
   add_foreign_key "games", "developers"
 end
