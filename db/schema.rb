2f76626b305293c9a1cec79369f18b4f8979547d
johnkdbell <johnkbell@hotmail.com>
2020-10-23 10:40:11 -0500

    Create Platform and GamePlatform models and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_152934) do
+ActiveRecord::Schema.define(version: 2020_10_23_153916) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_10_23_152934) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["game_id"], name: "index_game_genres_on_game_id"
     t.index ["genre_id"], name: "index_game_genres_on_genre_id"
+  end
+
+  create_table "game_platforms", force: :cascade do |t|
+    t.integer "game_id", null: false
+    t.integer "platform_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_game_platforms_on_game_id"
+    t.index ["platform_id"], name: "index_game_platforms_on_platform_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -45,7 +54,15 @@ ActiveRecord::Schema.define(version: 2020_10_23_152934) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "game_genres", "games"
   add_foreign_key "game_genres", "genres"
+  add_foreign_key "game_platforms", "games"
+  add_foreign_key "game_platforms", "platforms"
   add_foreign_key "games", "developers"
 end
